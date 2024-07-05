82581ffd19b35105e42a763bdf9b23a9ff77c43f
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2024-07-05 18:13:22 -0400

    more helper options

--- datasets.py
+++ datasets.py
@@ -20,7 +20,8 @@ def read_dataset(data_root, data_name, flag=None, conditioning_parameters=None):
 
 def read_preprocess_dataset(
     data_root, data_name, flag=None, conditioning_parameters=None,
-    norm_dict=None, invert_mask=True, dtype=torch.float32):
+    norm_dict=None, norm_data=True, invert_mask=True, return_numpy=False,
+    dtype=torch.float32):
     """ Read and preprocess a dataset
 
     Parameters
@@ -35,9 +36,13 @@ def read_preprocess_dataset(
         List of conditioning parameters to use
     norm_dict : dict, optional
         Dictionary containing normalization parameters
+    norm_data : bool, optional
+        Whether to normalize the data. Default is True.
     invert_mask : bool, optional
         Whether to invert the mask. This is necessary because the mask convention
         is different between torch and jax. Default is True.
+    return_numpy : bool, optional
+        Whether to return numpy arrays instead of torch tensors. Default is False.
     dtype : torch.dtype, optional
         Data type of the tensors. Default is torch.float32.
     """
@@ -48,33 +53,41 @@ def read_preprocess_dataset(
     mask = data['mask']
     cond = cond_table.values
 
-    # normalize the dat
-    if norm_dict is None:
-        mask_bool = mask.astype(bool)
-        x_mean = np.mean(x, axis=(0, 1), where=mask_bool[..., None])
-        x_std = np.std(x, axis=(0, 1), where=mask_bool[..., None])
-        cond_mean = np.mean(cond, axis=0)
-        cond_std = np.std(cond, axis=0)
-        norm_dict = {
-            'x_mean': x_mean,
-            'x_std': x_std,
-            'cond_mean': cond_mean,
-            'cond_std': cond_std
-        }
+    # normalize the data
+    if norm_data:
+        if norm_dict is None:
+            mask_bool = mask.astype(bool)
+            x_mean = np.mean(x, axis=(0, 1), where=mask_bool[..., None])
+            x_std = np.std(x, axis=(0, 1), where=mask_bool[..., None])
+            cond_mean = np.mean(cond, axis=0)
+            cond_std = np.std(cond, axis=0)
+            norm_dict = {
+                'x_mean': x_mean,
+                'x_std': x_std,
+                'cond_mean': cond_mean,
+                'cond_std': cond_std
+            }
+        else:
+            x_mean = norm_dict['x_mean']
+            x_std = norm_dict['x_std']
+            cond_mean = norm_dict['cond_mean']
+            cond_std = norm_dict['cond_std']
+        x = (x - x_mean + EPS) / (x_std + EPS)
+        cond = (cond - cond_mean + EPS) / (cond_std + EPS)
     else:
-        x_mean = norm_dict['x_mean']
-        x_std = norm_dict['x_std']
-        cond_mean = norm_dict['cond_mean']
-        cond_std = norm_dict['cond_std']
-    x = (x - x_mean + EPS) / (x_std + EPS)
-    cond = (cond - cond_mean + EPS) / (cond_std + EPS)
+        norm_dict = None
 
-    x = torch.tensor(x, dtype=dtype)
-    cond = torch.tensor(cond, dtype=dtype)
-    mask = torch.tensor(mask, dtype=torch.bool)
-    if invert_mask:
-        mask = ~mask
-    return x, cond, mask, norm_dict
+    if return_numpy:
+        if invert_mask:
+            mask = ~mask
+        return x, cond, mask, norm_dict
+    else:
+        x = torch.tensor(x, dtype=dtype)
+        cond = torch.tensor(cond, dtype=dtype)
+        mask = torch.tensor(mask, dtype=torch.bool)
+        if invert_mask:
+            mask = ~mask
+        return x, cond, mask, norm_dict
 
 def create_dataloader(x, cond, mask, **kwargs):
     """ Create a PyTorch DataLoader """
